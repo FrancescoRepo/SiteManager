65e21f3fbef7043fc7debff0298ec2920d458b4f
Francesco <francesco.colaianni96@gmail.com>
2017-05-28 11:54:43 +0200

    Aggiornati file Bootstrap Aggiornata tabella Siti

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -10,12 +10,15 @@
                 <header class="panel-heading">
                     Siti
                 </header>
-                <table class="table">
+                <table class="table text-center">
                     <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>Descrizione</th>
-                        <th>Azioni</th>
+                        <th class="text-center">Nome</th>
+                        <th class="text-center">Descrizione</th>
+                        @if(Auth::user()->usertype->Description == "ResponsabileAziendale")
+                            <th class="text-center">Modifica</th>
+                            <th class="text-center">Elimina</th>
+                        @endif
                     </tr>
                     </thead>
 
@@ -25,13 +28,14 @@
                         <tr class='clickable-row' data-href="{{route('sensors' ,  $site->id)}}">
                             <td><a href="{{route('sensors' ,  $site->id)}}">{{ $site->Name }}</a></td>
                             <td>{{ $site->Description }}</td>
-                            <td>
-                                <div class="btn-group">
-                                    <a class="btn btn-primary" href="#"><i class="icon_plus_alt2"></i></a>
-                                    <a class="btn btn-success" href="#"><i class="icon_check_alt2"></i></a>
-                                    <a class="btn btn-danger" href="#"><i class="icon_close_alt2"></i></a>
-                                </div>
-                            </td>
+                            @if(Auth::user()->usertype->Description == "ResponsabileAziendale")
+                                <td>
+                                    <p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p>
+                                </td>
+                                <td>
+                                    <p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p>
+                                </td>
+                            @endif
                         </tr>
 
                     @endforeach
@@ -41,5 +45,48 @@
             </section>
         </div>
     </div>
+
+    <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+                    <h4 class="modal-title custom_align" id="Heading">Modifica il sito</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input class="form-control " type="text" placeholder="Nome">
+                    </div>
+                    <div class="form-group">
+                        <textarea rows="2" class="form-control" placeholder="Descrizione"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer ">
+                    <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+                    <h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
+                </div>
+                <div class="modal-body">
+
+                    <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this Record?</div>
+
+                </div>
+                <div class="modal-footer ">
+                    <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
 
 @endsection
