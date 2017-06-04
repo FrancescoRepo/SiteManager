e6294c30e6c8266b9f5daf1d12bc41e9604bf7b1
Francesco <francesco.colaianni96@gmail.com>
2017-06-04 18:58:44 +0200

    Ripristinata modifica al modal sito e sensore javascript

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="shortcut icofn" href="img/favicon.png">
 
     <title>SiteManager - @yield('title')</title>
 
@@ -25,7 +25,6 @@
     <link href="{{ asset('css/widgets.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/style.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/style-responsive.css')}}" rel="stylesheet"/>
-    <link href="{{ asset('css/xcharts.min.css') }}" rel=" stylesheet"/>
     <link href="{{ asset('css/jquery-ui-1.10.4.min.css') }}" rel="stylesheet"/>
 </head>
 
@@ -65,32 +64,21 @@
 <script type="text/javascript" src="{{ asset('js/jquery-ui-1.9.2.custom.min.js') }}"></script>
 <!-- bootstrap -->
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js//bootstrap-toggle.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap-table.js') }}"></script>
 <!-- nice scroll -->
 <script src="{{ asset('js/jquery.scrollTo.min.js') }}"></script>
 <script src="{{ asset('js/jquery.nicescroll.js') }}" type="text/javascript"></script>
-<script src="{{ asset('js/owl.carousel.js') }}"></script>
 <!--script for this page only-->
-<script src="{{ asset('js/calendar-custom.js') }}"></script>
 <script src="{{ asset('js/jquery.rateit.min.js') }}"></script>
-<!-- custom select -->
-<script src="{{ asset('js/jquery.customSelect.min.js') }}"></script>
-<script src="{{ asset('assets/chart-master/Chart.js') }}"></script>
 
 <!--custome script for all page-->
 <script src="{{ asset('js/scripts.js') }}"></script>
 <!-- custom script for this page-->
-<script src="{{ asset('js/sparkline-chart.js') }}"></script>
-<script src="{{ asset('js/easy-pie-chart.js') }}"></script>
 <script src="{{ asset('js/jquery-jvectormap-1.2.2.min.js') }}"></script>
 <script src="{{ asset('js/jquery-jvectormap-world-mill-en.js') }}"></script>
-<script src="{{ asset('js/xcharts.min.js') }}"></script>
 <script src="{{ asset('js/jquery.autosize.min.js') }}"></script>
 <script src="{{ asset('js/jquery.placeholder.min.js') }}"></script>
-<script src="{{ asset('js/gdp-data.js') }}"></script>
 <script src="{{ asset('js/morris.min.js') }}"></script>
-<script src="{{ asset('js/sparklines.js') }}"></script>
 <script src="{{ asset('js/charts.js') }}"></script>
 <script src="{{ asset('js/jquery.slimscroll.min.js') }}"></script>
 
@@ -186,6 +174,10 @@
         $('#editModalID').attr('value', site[0].trim());
         $('#editModalName').attr('value', site[1].trim());
         $('#editModalDescription').attr('value', site[2].trim());
+        $('#editModalAddressID').attr('value', site[3].trim());
+        $('#editModalStreet').attr('value', site[4].trim());
+        $('#editModalStreetNumber').attr('value', site[5].trim());
+        $('#editModalZipCode').attr('value', site[6].trim());
     });
 
     $('#deleteSiteModal').on('show.bs.modal', function(e) {
@@ -201,6 +193,8 @@
         $('#editModalLongitude').attr('value', sensor[3].trim());
         $('#editModalMaxVal').attr('value', sensor[4].trim());
         $('#editModalMinVal').attr('value', sensor[5].trim());
+        $('#editModalBrand option').filter(function(){ return $.trim( $(this).text() ) == sensor[6].trim();}).attr('selected', true);
+        $('#editModalType option').filter(function(){ return $.trim( $(this).text() ) == sensor[7].trim();}).attr('selected', true)
     });
 
     $('#deleteSensorModal').on('show.bs.modal', function(e) {
@@ -215,6 +209,10 @@
         var ID = $('#editModalID').val();
         var Name = $('#editModalName').val();
         var Description = $('#editModalDescription').val();
+        var AddressID = $('#editModalAddressID').val();
+        var Street = $('#editModalStreet').val();
+        var StreetNumber = $('#editModalStreetNumber').val();
+        var ZipCode = $('#editModalZipCode').val();
 
         $.ajax({
            url: '{{ route('editSite') }}',
@@ -222,7 +220,11 @@
                _token: "{{ csrf_token() }}",
                id: ID,
                Name: Name,
-               Description : Description
+               Description : Description,
+               AddressID: AddressID,
+               Street: Street,
+               StreetNumber: StreetNumber,
+               ZipCode: ZipCode
            },
            type: 'POST',
            success: function(data) {
@@ -271,6 +273,8 @@
         var Longitude = $('#editModalLongitude').val();
         var MaxVal = $('#editModalMaxVal').val();
         var MinVal = $('#editModalMinVal').val();
+        var brand_id = $('#editModalBrand').val();
+        var type_id = $('#editModalType').val();
 
         $.ajax({
             url: '{{ route('editSensor') }}',
@@ -281,11 +285,13 @@
                 Latitude : Latitude,
                 Longitude: Longitude,
                 MaxValue: MaxVal,
-                MinValue: MinVal
+                MinValue: MinVal,
+                brand_id: brand_id,
+                type_id: type_id
             },
             type: 'POST',
             success: function(data) {
-                console.log(data['data']);
+                //console.log(data['data']);
                 location.reload();
 
                 //Capire come poter aggiornare la riga mantenendo il link
